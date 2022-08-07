a9d7cc258f82aa80df6634163dcd998dd9dd6c5e
Stephen Novelli <snovelli1021@gmail.com>
2022-08-07 12:58:12 -0700

    Setting up local storage outline and current time moment to use for changing calendar display

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,34 @@
-var currentDT = moment().format("dddd MMM D, YYYY");
-    console.log('Current Date: ' + currentDT);
+var saveBtn = document.querySelector(".saveBtn");
+var cardSpan = document.querySelector(".card");
+var task = document.querySelector("#task").value;
 
-$("#currentDT").text(currentDT);   
+var currentDT = moment().format("dddd MMM D, YYYY");
+    console.log("Current Date: " + currentDT);
+
+$("#currentDT").text(currentDT); 
+
+var currentTime = moment().format("h");
+    console.log("Current Hour: " + currentTime)
+
+$("#currentTime").text(currentTime);
+
+
+//document.querySelector(".row").className = "present"; 
+
+
+
+function displayTask() {
+
+    var task = localStorage.getItem("task");
+    cardSpan.textContent = task;
+    //checking to see if var task recieves a value
+    console.log(task);
+}
+
+
+saveBtn.addEventListener("click", function(event) {
+    localStorage.setItem("task", task);
+    console.log(task);
+    event.preventDefault();
+    displayTask();
+});
