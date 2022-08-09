e44cf6ac979f56e308b0ed3137bc3b4e9fffdd7c
Stephen Novelli <snovelli1021@gmail.com>
2022-08-08 20:59:02 -0700

    Submitting assignment 5

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 UW Coding Bootcamp assignment 5: Work Day Scheduler
-
+Building a work day scheduler that allows a user to type in a task and have it populate on screen. The time blocks are supposed to change colors depending on the time of day.
 
 
 Github Repository Link:
@@ -9,3 +9,5 @@ Github Repository Link:
 Github Pages Link:
 
 
+
+![my screenshot](./assets/screenshot.jpg)
--- assets/script.js
+++ assets/script.js
@@ -19,27 +19,31 @@ var timeBlockArray = Array.from(timeBlock)
 //Checking to make sure my node list was made into an array
 console.log(timeBlockArray);
 
-function changeColor() {
-for (var i =0; i < timeBlockArray.length; i++) {
- }
+// function changeColor() {
+// for (var i =0; i < timeBlockArray.length; i++) {
+//  }
 
-   if (currentTime === timeBlockArray) {
-     //timeBlock row turns red
-       document.querySelector(".row").className= "present";
-    }else if (currentTime > timeBlockArray) {
-     //timeBlock row turns gray
-       document.querySelector(".row").className= "past";
-    }else { (currentTime < timeBlockArray)
-     //timeBlock row turns green
-      document.querySelector(".row").className= "future";
-    }
-}
+//    //if (currentTime === timeBlockArray) {
+//      //timeBlock row turns red
+//        document.querySelector(".row").className= "present";
+//     //}else if (currentTime > timeBlockArray) {
+//      //timeBlock row turns gray
+//        document.querySelector(".row").className= "past";
+//     }else { (currentTime < timeBlockArray)
+//      //timeBlock row turns green
+//       document.querySelector(".row").className= "future";
+//     }
+// }
 
-changeColor();
+// changeColor();
+
+//Local Stprage only works for 8:00am Time Block
 
 function displayTask() {
     var inputTask = localStorage.getItem("task");
     cardSpan.textContent = inputTask;
+    // for (var i = 0; i < inputTaskArray.length; i++){    
+    // }
     //checking to see if var inputTask recieves a value
     console.log(inputTask);
 }
